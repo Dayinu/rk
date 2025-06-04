653ec4f831c307eeb603a5213bcdc063bb33baf4
Kote <IvanovaVladaV@yandex.ru>
2025-06-04 20:05:39 +0300

    Update PayByCreditCard.h

--- include/Payment/PayByCreditCard.h
+++ include/Payment/PayByCreditCard.h
@@ -5,6 +5,14 @@
 /**
  * Concrete strategy. Implements credit card payment method.
  */
+#pragma once
+
+#include "PaymentStrategy.h"  // Базовый класс
+#include <memory>             // Для std::unique_ptr
+
+// Forward declaration вместо #include "CreditCard.h"
+class CreditCard;
+
 class PayByCreditCard : public PaymentStrategy {
 private:
     std::unique_ptr<CreditCard> m_card;
